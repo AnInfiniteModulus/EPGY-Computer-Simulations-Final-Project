305664a8ed13df3ddf5fd1e35679b956945fa70e
Andrew Ding <andrew.ding@bsg.org.cn>
2015-07-06 10:22:15 -0700

    Lots of code. AI essentially done. Difficulty scaling done
    Lots of variables. Created player object, game state, array of obstacles
    (which need to be filled with actual links). Gametime variable, boolean
    variable isGameOver, score, and update function. Oh baby we're ahead of
    schedule.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,47 @@ canvas.height = 480; // Height of canvas in pixels, should be edited later
 document.body.appendChild(canvas);
 //Canvas creation finished
 
+//Variables with obstacle sprites TODO: ACTUALLY LINK THEM TO sprites
+var obstacles = ['','','',''] //0 should be square, 1: rectangle, 2: circle, 3:  triangle
+
+//Game state
+var player = { //Player object containing position and sprite
+  pos: [0, 0],  //Player coordinates
+  sprite: new Sprite(); //TODO: Give sprite parameters and file
+};
+
+var liveObstacles = []; //Array of obstacles
+
+var gameTime = 0; //How long the game has been going
+var isGameOver; //Boolean value checking whether game is over or not
+
+//Score
+var score = 0;
+var scoreEl = document.getElementById('score');
+
+//Update function
+function update(dt) {
+
+  gameTime += dt;
+  handleInput(dt);
+  updateEntities(dt);
+
+  //AI for creating obstacles
+  if(Math.random() < 1 - Math.pow(.993, gameTime)){ //Makes the game harder and harder
+    liveObstacles.push({ //Puts a new obstacle in the obstacles array
+      pos: [canvas.width, //Setting a random position
+            Math.random() * (canvas.height - 64)], //Value of 64 as that is the height of the sprite
+      sprite: new Sprite(obstacles[Math.random()*5|0], [0, 0], [64, 64], 6, [0] ) //Creating a new random sprite from the obstacles array
+      //TODO: HOLY CRAP WHAT ARE THOSE PARAMETERS. THE DOCUMENTATION IS SO USELESS. THE HELL?!?
+    })
+  }
+
+  checkCollisions();
+
+  scoreEl.innerHTML = score;
+
+};
+
 //The main game loop
 var lastTime;
 function main() {
